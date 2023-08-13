6092b20e892089562f52e71414a505c8b3aa1364
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-08-13 13:12:46 +0545

    added empty state to user

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+const EmptyState = () => {
+  return (
+    <div
+      className="
+    px-4
+    py-10
+    sm:px-6
+    lg:px-8
+    flex
+    h-full
+    justify-center
+    items-center
+    bg-gray-100
+  "
+    >
+      <div className="flex flex-col text-center items-center">
+        <h3
+          className="
+         mt-2
+         text-2xl
+         font-semibold
+         text-gray-900
+        "
+        >
+          Selecte a chat or start a new converstion
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyState;
