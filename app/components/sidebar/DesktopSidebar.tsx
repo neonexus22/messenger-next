2dcfd60c4dfb7530cf75eaa628e7ddf947ad06c4
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-08-13 16:18:48 +0545

    added Avatar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,31 +1,38 @@
 "use client";
 
-import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
-import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
 
-const DesktopSidebar = () => {
+import useRoutes from "@/app/hooks/useRoutes";
+import DesktopItem from "./DesktopItem";
+import Avatar from "../Avatar";
+
+interface DesktopSidebarProps {
+  currentUser?: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div
       className="
-  hidden
-  lg:fixed
-  lg:inset-y-0
-  lg:left-0
-  lg:z-40
-  lg:w-20
-  xl:px-6
-  lg:overflow-y-auto
-  lg:bg-white
-  lg:border-r-[1px]
-  lg:pb-4
-  lg:flex
-  lg:flex-col
-  justify-between
-  "
+        hidden
+        lg:fixed
+        lg:inset-y-0
+        lg:left-0
+        lg:z-40
+        lg:w-20
+        xl:px-6
+        lg:overflow-y-auto
+        lg:bg-white
+        lg:border-r-[1px]
+        lg:pb-4
+        lg:flex
+        lg:flex-col
+        justify-between
+        "
     >
       <nav className="mt-4 flex flex-col justify-between">
         <ul role="list" className="flex flex-col items-center space-y-1">
@@ -40,6 +47,14 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
