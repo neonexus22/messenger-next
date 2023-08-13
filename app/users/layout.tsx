f2bda71cd13f99af13571b480de55f11476509c1
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-08-13 17:13:53 +0545

    added user list sidebar

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Sidebar from "../components/sidebar/Sidebar";
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 interface UserLayoutProps {
   children: React.ReactNode;
 }
 
 const UsersLayout = async ({ children }: UserLayoutProps) => {
+  const users = await getUsers();
   return (
     // @ts-expect-error Server Component
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
